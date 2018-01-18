b1ded54662953d3d57aa0ce31bc3f3c42324e6cc
liuke <liuke@iwaimai.baidu.com>
2018-01-18 16:33:50 +0800

    1.class loader learing

---
+++ java_learing/src/main/java/classloader/JavaClassLoader.java
@@ -0,0 +1,14 @@
+package classloader;
+
+/**
+ * Created by kenan on 18/1/18.
+ * 它的主要作用就是通过多种类加载器（ClassLoader）来查找和加载Class文件到 Java 虚拟机中。
+ Java中的类加载器主要有两种类型，系统类加载和自定义类加载器。其中系统类加载器包括3种，
+ 分别是Bootstrap ClassLoader、 Extensions ClassLoader和 App ClassLoader。
+
+ classloader 重要
+
+ */
+
+public class JavaClassLoader {
+}
