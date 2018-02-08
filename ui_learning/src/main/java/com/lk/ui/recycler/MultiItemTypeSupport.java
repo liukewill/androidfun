d3688af259a5201867632b1820c5ae2d488381e2
liuke <liuke@iwaimai.baidu.com>
2018-02-08 15:41:59 +0800

    1.rebuild module

--- ui_learning/src/main/java/com/lk/ui/recycler/MultiItemTypeSupport.java
+++
@@ -1,9 +0,0 @@
-package com.lk.ui.recycler;
-
-/**
- * Created by kenan on 17/5/9.
- */
-public interface MultiItemTypeSupport<T> {
-    int getLayoutId(int itemType);
-    int getItemViewType(int position,T t);
-}
