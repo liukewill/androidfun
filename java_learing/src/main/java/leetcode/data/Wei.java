8a40e4632f4cfa532378ebf0b90a145fa2d80b85
liuke <liuke@iwaimai.baidu.com>
2018-02-27 21:10:28 +0800

    1.rebuild module

---
+++ java_learing/src/main/java/leetcode/data/Wei.java
@@ -0,0 +1,26 @@
+package leetcode.data;
+
+import java.util.ArrayList;
+
+/**
+ * Created by kenan on 18/2/8.
+ */
+
+public class Wei {
+
+    public static void main (String []args){
+        ArrayList arrayList=new ArrayList();
+        arrayList.add("dd");
+        arrayList.add(3);
+        arrayList.add(3);
+        arrayList.add(3);
+        arrayList.add(3);
+
+
+        Object [] a={3,"d"};
+        for (Object o : a) {
+            System.out.println(o.toString());
+        }
+
+    }
+}
